165eb8bf6d3afb6c47706086ece37432f35a4448
do5do <do151015@gmail.com>
2023-07-18 01:06:28 +0900

    0718!

--- src/baekJoon/Australian_voting_4419.java
+++ src/baekJoon/Australian_voting_4419.java
@@ -12,34 +12,35 @@ public class Australian_voting_4419 {
         int n = Integer.parseInt(br.readLine()); // 후보자 수
         List<String> candidates = new ArrayList<>(); // 후보자
         Map<String, Integer> mappedCandidates = new HashMap<>(); // 후보자 이름, 투표수
-        int[][] votes = new int[1000][n]; // 투표 내역
-        int voterNum = 0; // 유권자 수
+        List<int[]> votes = new ArrayList<>(); // 투표 내역
 
         for (int i = 0; i < n; i++) {
             mappedCandidates.put(br.readLine(), 0);
             candidates.add(br.readLine());
         }
 
-        // set 투표 내역
+        // 투표 내역 세팅
         StringTokenizer st;
         String input;
-        while ((input = br.readLine()) != null) {
+        while ((input = br.readLine()) != null && input.length() > 0) {
             st = new StringTokenizer(input);
-
+            int[] arr = new int[n];
             for (int i = 0; i < n; i++) {
-                votes[voterNum][i] = Integer.parseInt(st.nextToken());
+                arr[i] = Integer.parseInt(st.nextToken()) - 1; // index 맞춰주기 위해 -1을 해준다.
             }
-            voterNum++;
+            votes.add(arr);
         }
 
         // 득표수 세기
-        for (int i = 0; i < voterNum; i++) {
-            String name = candidates.get(votes[i][0]);
+        for (int[] vote : votes) {
+            String name = candidates.get(vote[0]);
             mappedCandidates.put(name, mappedCandidates.get(name) + 1);
         }
 
-        while (true) {
+        System.out.println(mappedCandidates);
 
-        }
+//        while (true) {
+//
+//        }
     }
 }
