eecaba19e280b56861e168fcbc0fa86e3b4cbfcc
dohee Kim <do151015@gmail.com>
2024-05-11 09:51:31 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         StringBuilder sb = new StringBuilder();
-        for (int i = my_string.length() - 1; i >= 0; i--) {
-            sb.append(my_string.charAt(i));
-        }
-        return sb.toString();
+        sb.append(my_string);
+        return sb.reverse().toString();
     }
 }
