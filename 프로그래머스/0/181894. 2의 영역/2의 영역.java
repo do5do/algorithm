b866effbccab32648829c593bdbf14accbab01e0
dohee Kim <do151015@gmail.com>
2024-05-11 10:21:16 +0900

    [level 0] Title: 2의 영역, Time: 1.81 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -2,27 +2,18 @@ class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
         int start = -1;
+        int end = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 2) {
-                start = i;
-                break;
-            }
-        }
-
-        int end = -1;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] == 2) {
+                if (start == -1) {
+                    start = i;
+                }
                 end = i;
-                break;
             }
         }
 
-        if (start == -1 && end == -1) {
+        if (start == -1) {
             return new int[] {-1};
-        } else if (start != -1 && end == -1) {
-            return new int[] {arr[start]};
-        } else if (start == -1 && end != -1) {
-            return new int[] {arr[end]};
         } else {
             answer = new int[end - start + 1];
         }
