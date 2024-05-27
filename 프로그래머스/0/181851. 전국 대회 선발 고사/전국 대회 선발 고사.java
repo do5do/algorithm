1ea2b8d74bf8680f6e7f3906266d19fe7707dee0
dohee Kim <do151015@gmail.com>
2024-05-27 14:58:22 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.22 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        for (int i = 0; i < rank.length; i++) {
+        for (int i = 0; i < attendance.length; i++) {
             if (attendance[i]) {
                 pq.offer(new int[] {i, rank[i]});
             }
