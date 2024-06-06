9fae68857146df14e2551b607819aade332263bd
dohee Kim <do151015@gmail.com>
2024-06-06 10:20:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        for (String[] data : db) {
+            if (id_pw[0].equals(data[0]) && id_pw[1].equals(data[1])) {
+                answer = "login";
+                break;
+            } else if (id_pw[0].equals(data[0]) && !id_pw[1].equals(data[1])) {
+                answer = "wrong pw";
+            }
+        }
+        return answer;
+    }
+}
