52066840c0ace537191d2d2a48b09de5d6bd6ff3
dohee Kim <do151015@gmail.com>
2024-07-29 21:33:46 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if (n == 0) {
-            return 0;
-        }
-        
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 answer += i;
             }
         }
-        return answer + 1;
+        return answer + 1 + n;
     }
 }
