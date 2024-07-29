888c23c9a3cf4b7a3eca09e38cea5163890f0c73
dohee Kim <do151015@gmail.com>
2024-07-29 12:25:43 +0900

    [level 1] Title: 문자열 나누기, Time: 0.97 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,30 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int i = 0;
-        int lastIdx = 0;
+        int cnt = 0;
+        int x = s.charAt(0);
         
-        while (i < s.length()) {
-            char x = s.charAt(i);
-            int xCnt = 0;
-            int oCnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (cnt == 0) {
+                x = s.charAt(i);
+            }
             
-            for (; i < s.length(); i++) {
-                if (x == s.charAt(i)) {
-                    xCnt++;
-                } else {
-                    oCnt++;
-                }
-                
-                if (xCnt == oCnt) {
-                    answer++;
-                    lastIdx = ++i;
-                    break;
-                }
+            if (x == s.charAt(i)) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            
+            if (cnt == 0) {
+                answer++;
             }
         }
         
-        if (lastIdx < s.length()) {
+        if (cnt > 0) {
             answer++;
         }
         return answer;
