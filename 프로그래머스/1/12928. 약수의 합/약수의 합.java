48c945512f22c316477cd409b7be419b54700527
dohee Kim <do151015@gmail.com>
2024-07-29 21:34:55 +0900

    Update 약수의 합.java

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        
         for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 answer += i;
