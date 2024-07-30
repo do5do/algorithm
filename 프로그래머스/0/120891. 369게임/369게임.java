909e7e77db2dd7e4a8f4a36f4f0926daff027cbd
dohee Kim <do151015@gmail.com>
2024-07-30 14:21:09 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        while (order != 0) {
+        while (order > 0) {
             int rest = order % 10;
-            order /= 10;
-            
             if (rest == 3 || rest == 6 || rest == 9) {
                 answer++;
             }
+            order /= 10;
         }
         return answer;
     }
