e05a367d9cbee568f941a209c209c43afd0cf382
dohee Kim <do151015@gmail.com>
2024-08-21 11:29:19 +0900

    [level 2] Title: 호텔 대실, Time: 25.53 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] book_time) {
+        int answer = 1;
+        List<Reservation> reservations = new ArrayList<>();
+        
+        for (String[] book : book_time) {
+            String[] startArr = book[0].split(":");
+            int start = Integer.parseInt(startArr[0]) * 60 + Integer.parseInt(startArr[1]);
+            
+            String[] endArr = book[1].split(":");
+            int end = Integer.parseInt(endArr[0]) * 60 + Integer.parseInt(endArr[1]);
+            
+            reservations.add(new Reservation(start, end));
+        }
+        
+        Collections.sort(reservations);
+        List<Integer> rooms = new ArrayList<>();
+        
+        for (int i = 0; i < reservations.size(); i++) {
+            Reservation cur = reservations.get(i);
+            boolean newRoom = true;
+            
+            for (int j = 0; j < rooms.size(); j++) {
+                if (rooms.get(j) <= cur.start) {
+                    newRoom = false;
+                    rooms.set(j, cur.end + 10);
+                    break;
+                }
+            }
+            
+            if (newRoom) {
+                answer++;
+                rooms.add(cur.end + 10);
+            }
+        }
+        
+        return rooms.size();
+    }
+    
+    static class Reservation implements Comparable<Reservation> {
+        int start;
+        int end;
+        
+        public Reservation(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+        
+        public int compareTo(Reservation o) {
+            return Integer.compare(this.start, o.start);
+        }
+    }
+}
