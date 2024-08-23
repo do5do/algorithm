5087479ab3c541106ef207650574375aa77160bd
dohee Kim <do151015@gmail.com>
2024-08-23 11:33:55 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 313.72 ms, Memory: 161 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(numbers[numbers.length - 1]);
+        answer[numbers.length - 1] = -1;
+        
+        for (int i = numbers.length - 2; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= numbers[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                answer[i] = -1;
+            } else {
+                answer[i] = stack.peek();
+            }
+            
+            stack.push(numbers[i]);
+        }
+        return answer;
+    }
+}
