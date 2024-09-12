fd5e64b19f585a24004091bca3785f9e35470b29
dohee Kim <do151015@gmail.com>
2024-09-12 11:15:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 96.44 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#qna) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 110.73 ms
+메모리: 138 MB, 시간: 96.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 28일 18:32:07
+2024년 09월 12일 11:15:15
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,44 +2,48 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -1;
+        int answer = 0;
         long sum1 = 0;
         long sum2 = 0;
-        int cnt = 0;
         Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q2 = new LinkedList<>();
-
+        
         for (int i = 0; i < queue1.length; i++) {
-            q1.offer(queue1[i]);
             sum1 += queue1[i];
-
-            q2.offer(queue2[i]);
             sum2 += queue2[i];
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
         }
-
-        while (!q1.isEmpty() && !q2.isEmpty()) {
-            if (cnt >= (queue1.length + queue2.length) * 3) {
-                break;
+        
+        long target = (sum1 + sum2) / 2;
+        
+        while (answer <= (queue1.length) * 3) {
+            if (sum1 > sum2) {
+                if (!q1.isEmpty()) {
+                    int item = q1.poll();
+                    q2.offer(item);
+                    sum1 -= item;
+                    sum2 += item;
+                    answer++;
+                }
+            } else if (sum1 < sum2) {
+                if (!q2.isEmpty()) {
+                    int item = q2.poll();
+                    q1.offer(item);
+                    sum1 += item;
+                    sum2 -= item;
+                    answer++;
+                }
             }
             
-            if (sum1 < sum2) {
-                int num = q2.poll();
-                q1.offer(num);
-                sum1 += num;
-                sum2 -= num;
-            } else if (sum1 > sum2) {
-                int num = q1.poll();
-                q2.offer(num);
-                sum1 -= num;
-                sum2 += num;
-            } else {
-                answer = cnt;
-                break;
+            if (sum1 == sum2 && sum1 == target) {
+                return answer;
             }
-
-            cnt++;
         }
-
+        
+        if (sum1 != sum2) {
+            return -1;
+        }
         return answer;
     }
 }
