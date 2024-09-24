cda4018a95b5ae0f771397c8063407d2525ab75a
dohee Kim <do151015@gmail.com>
2024-09-24 20:50:06 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.06 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,6 +1,27 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
+
+        while (storey > 0) {
+            int rest = storey % 10;
+            storey /= 10;
+            
+            if (rest >= 6) {
+                answer += 10 - rest;
+                storey++;
+            } else if (rest <= 4) {
+                answer += rest;
+            } else { // 5일 때
+                int prev = storey % 10;
+                if (prev >= 5) {
+                    answer += 10 - rest;
+                    storey++;
+                } else {
+                    answer += rest;
+                }
+            }
+        }
+        
         return answer;
     }
 }
