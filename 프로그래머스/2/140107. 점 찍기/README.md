2902a47b0703fe937cec92a5853fd7ac3a788a8a
dohee Kim <do151015@gmail.com>
2024-09-26 10:38:17 +0900

    [level 2] Title: 점 찍기, Time: 19.82 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/README.md
+++ 프로그래머스/2/140107. 점 찍기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점 찍기 - 140107 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140107#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140107) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.08 ms
+메모리: 80.1 MB, 시간: 19.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 17일 12:38:04
+2024년 09월 26일 10:38:15
 
 ### 문제 설명
 
--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,9 +1,9 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        for (int i = 0; i <= d; i += k) { // x 축
-            double y = Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2)); // 현재 x에 대한 최대 y좌표
-            answer += (long) (y / k) + 1;
+        for (int x = 0; x <= d; x += k) {
+            long maxY = (long) Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
+            answer += (maxY / k) + 1;
         }
         return answer;
     }
