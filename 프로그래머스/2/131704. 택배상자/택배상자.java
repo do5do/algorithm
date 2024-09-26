e92752e49d97a279e7d2f08f7f52310309c53214
dohee Kim <do151015@gmail.com>
2024-09-26 16:46:43 +0900

    [level 2] Title: 택배상자, Time: 203.86 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int idx = 0;
+        
+        for (int i = 1; i <= order.length; i++) {
+            stack.push(i);
+            
+            while (!stack.isEmpty()) {
+                if (stack.peek() == order[idx]) {
+                    stack.pop();
+                    idx++;
+                    answer++;
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
