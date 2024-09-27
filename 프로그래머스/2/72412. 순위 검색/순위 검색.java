39d72d296700983a6e5fb368031520945d6c802d
dohee Kim <do151015@gmail.com>
2024-09-27 13:17:43 +0900

    [level 2] Title: 순위 검색, Time: 1264.92 ms, Memory: 313 MB -BaekjoonHub

---
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    Map<String, List<Integer>> map = new HashMap<>();
+    
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        
+        for (int i = 0; i < info.length; i++) {
+            String[] split = info[i].split(" ");
+            dfs(0, "", split);
+        }
+        
+        for (String key : map.keySet()) {
+            Collections.sort(map.get(key));
+        }
+        
+        for (int i = 0; i < query.length; i++) {
+            String str = query[i].replaceAll(" and ", "");
+            String[] split = str.split(" ");
+            String key = split[0];
+            int cnt = 0;
+            
+            if (map.containsKey(key)) {
+                cnt = binarySearch(key, Integer.parseInt(split[1]));
+            }
+            
+            answer[i] = cnt;
+        }
+        
+        return answer;
+    }
+    
+    public int binarySearch(String key, int score) {
+        List<Integer> scores = map.get(key);
+        
+        int start = 0;
+        int end = scores.size() - 1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (scores.get(mid) < score) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return scores.size() - start;
+    }
+    
+    public void dfs(int depth, String key, String[] arr) {
+        if (depth == 4) {
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            
+            map.get(key).add(Integer.parseInt(arr[4]));
+            return;
+        }
+        
+        dfs(depth + 1, key + "-", arr);
+        dfs(depth + 1, key + arr[depth], arr);
+    }
+}
