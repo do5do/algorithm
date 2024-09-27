d15b27a28c033f0a7fbce06f80592aa6a888b2a0
dohee Kim <do151015@gmail.com>
2024-09-27 17:58:58 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.12 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -2,47 +2,25 @@ import java.util.*;
 
 class Solution {
     boolean[] visited;
-    List<int[]> selected = new ArrayList<>();
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
     
     public int solution(int[] cards) {
         int answer = 0;
+        visited = new boolean[cards.length];
         
-        // 조합. 2개 선택
-        select(0, cards.length, 2, new boolean[cards.length]);
-        
-        for (int[] nums : selected) {
-            visited = new boolean[cards.length];
-            
-            int first = find(cards[nums[0]] - 1, 0, cards);
-            int second = find(cards[nums[1]] - 1, 0, cards);
-            
-            answer = Math.max(answer, first * second);
-        }
-        
-        return answer;
-    }
-    
-    public void select(int start, int n, int r, boolean[] visited) {
-        if (r == 0) {
-            int[] result = new int[2];
-            int idx = 0;
-            
-            for (int i = 0; i < n; i++) {
-                if (visited[i]) {
-                    result[idx++] = i;
-                    System.out.print(i + ", ");
-                }
+        for (int card : cards) {
+            if (visited[card - 1]) {
+                continue;
             }
-            System.out.println();
-            selected.add(result);
-            return;
+            
+            int num = find(card - 1, 0, cards);
+            pq.add(num);
         }
         
-        for (int i = start; i < n; i++) {
-            visited[i] = true;
-            select(i + 1, n, r - 1, visited);
-            visited[i] = false;
+        if (pq.size() < 2) {
+            return answer;
         }
+        return pq.poll() * pq.poll();
     }
     
     public int find(int idx, int cnt, int[] cards) {
