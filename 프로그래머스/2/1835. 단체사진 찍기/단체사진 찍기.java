a0aaae80d6abc9fe34431424973fd793dd3a7f60
dohee Kim <do151015@gmail.com>
2024-09-30 22:00:54 +0900

    [level 2] Title: 단체사진 찍기, Time: 955.40 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,49 +1,42 @@
 class Solution {
-    int answer = 0;
-    char[] names = new char[] {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    char[] member = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
     boolean[] visited = new boolean[8];
+    int answer;
     
     public int solution(int n, String[] data) {
-        dfs(8, 0, new char[8], data);
+        dfs(0, new char[8], data);
         return answer;
     }
     
-    public void dfs(int n, int depth, char[] result, String[] data) {
-        if (depth == n) {
+    public void dfs(int depth, char[] result, String[] data) {
+        if (depth == member.length) {
             for (String d : data) {
                 char a = d.charAt(0);
                 char b = d.charAt(2);
                 char mark = d.charAt(3);
-                int val = d.charAt(4) - '0';
+                int num = d.charAt(4) - '0';
+                int aIdx = 0;
+                int bIdx = 0;
                 
-                int aIdx = -1;
-                int bIdx = -1;
-                for (int i = 0; i < n; i++) {
+                for (int i = 0; i < result.length; i++) {
                     if (result[i] == a) {
                         aIdx = i;
-                    }
-                    
-                    if (result[i] == b) {
+                    } else if (result[i] == b) {
                         bIdx = i;
-                    }
-                    
-                    if (aIdx != -1 && bIdx != -1) {
-                        break;
                     }
                 }
                 
                 int diff = Math.abs(aIdx - bIdx) - 1;
-                
                 if (mark == '=') {
-                    if (diff != val) {
+                    if (diff != num) {
                         return;
                     }
-                } else if (mark == '<') {
-                    if (diff >= val) {
+                } else if (mark == '>') {
+                    if (diff <= num) {
                         return;
                     }
                 } else {
-                    if (diff <= val) {
+                    if (diff >= num) {
                         return;
                     }
                 }
@@ -52,11 +45,11 @@ class Solution {
             return;
         }
         
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < member.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                result[depth] = names[i];
-                dfs(n, depth + 1, result, data);
+                result[depth] = member[i];
+                dfs(depth + 1, result, data);
                 visited[i] = false;
             }
         }
