e52e075bddd63e8cc442776180b493183a642376
dohee Kim <do151015@gmail.com>
2024-10-01 18:44:07 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 16.17 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,9 +1,9 @@
 class Solution {
     char[][] matrix;
     boolean[][] check;
+    int answer = 0;
     
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
         matrix = new char[m][n];
 
         for (int i = 0; i < m; i++) {
@@ -12,20 +12,18 @@ class Solution {
             }
         }
 
+        step(m, n);
+        count(m, n);
+        if (answer == 0) {
+            return answer;
+        }
+
         while (true) {
-            step(m, n);
-            int cnt = count(m, n);
-            if (cnt == 0) {
-                break;
-            }
-            answer += cnt;
-            
             // 보드판 정리
             for (int i = m - 1; i >= 1; i--) {
                 for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == '-') {
-                        // 최상단까지 탐색
-                        for (int k = i - 1; k >= 0; k--) {
+                    if (check[i][j]) {
+                        for (int k = i - 1; k >= 0; k--) { // 최상단까지 탐색
                             if (matrix[k][j] != '-') {
                                 matrix[i][j] = matrix[k][j];
                                 matrix[k][j] = '-';
@@ -35,36 +33,43 @@ class Solution {
                     }
                 }
             }
+
+            boolean isNext = step(m, n);
+            count(m, n);
+            
+            if (!isNext) {
+                break;
+            }
         }
 
         return answer;
     }
 
-    public int count(int m, int n) {
-        int cnt = 0;
+    public void count(int m, int n) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (check[i][j]) {
-                    cnt++;
+                    answer++;
                     matrix[i][j] = '-';
                 }
             }
         }
-        return cnt;
     }
 
-    public void step(int m, int n) {
+    public boolean step(int m, int n) {
         check = new boolean[m][n];
+        boolean flag = false;
 
         for (int i = 0; i < m - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
                 if (matrix[i][j] == '-') {
                     continue;
                 }
-                
+
                 if (matrix[i][j] == matrix[i + 1][j]
                         && matrix[i + 1][j] == matrix[i][j + 1]
                         && matrix[i][j + 1] == matrix[i + 1][j + 1]) {
+                    flag = true;
                     check[i + 1][j] = true;
                     check[i][j + 1] = true;
                     check[i + 1][j + 1] = true;
@@ -72,5 +77,6 @@ class Solution {
                 }
             }
         }
+        return flag;
     }
 }
