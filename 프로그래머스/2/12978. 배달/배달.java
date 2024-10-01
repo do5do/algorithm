5b8819b25e8e5253fed0c43ac55cd3ca1de1b1d8
dohee Kim <do151015@gmail.com>
2024-10-01 16:57:47 +0900

    [level 2] Title: 배달, Time: 6.42 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,27 +1,42 @@
 import java.util.*;
 
 class Solution {
+    List<List<int[]>> graph = new ArrayList<>();
+    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        List<int[]>[] graph = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            graph[i] = new ArrayList<>();
+        
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
         
-        for (int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-            int weight = road[i][2];
-            graph[a].add(new int[] {b, weight});
-            graph[b].add(new int[] {a, weight});
+        for (int[] r : road) {
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
+            graph.get(a).add(new int[] {b, c});
+            graph.get(b).add(new int[] {a, c});
         }
-        
+
         int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
-        boolean[] visited = new boolean[N + 1];
+        
+        bfs(dist, N);
+        
+        for (int i = 1; i < dist.length; i++) {
+            if (dist[i] <= K) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void bfs(int[] dist, int n) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[1] - y[1]);
         pq.offer(new int[] {1, 0});
+        dist[1] = 0;
+        boolean[] visited = new boolean[n + 1];
         
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
@@ -31,19 +46,12 @@ class Solution {
             }
             visited[cur[0]] = true;
             
-            for (int[] node : graph[cur[0]]) {
-                if (dist[node[0]] > node[1] + cur[1]) {
-                    dist[node[0]] = node[1] + cur[1];
+            for (int[] node : graph.get(cur[0])) {
+                if (dist[node[0]] > cur[1] + node[1]) {
+                    dist[node[0]] = cur[1] + node[1];
                     pq.offer(new int[] {node[0], dist[node[0]]});
                 }
             }
         }
-        
-        for (int i = 1; i < dist.length; i++) {
-            if (dist[i] <= K) {
-                answer++;
-            }
-        }
-        return answer;
     }
 }
