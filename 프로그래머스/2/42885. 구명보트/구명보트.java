976a52af0387258579c7cabede992cb8c13da147
dohee Kim <do151015@gmail.com>
2024-10-07 10:39:44 +0900

    [level 2] Title: 구명보트, Time: 10.93 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,21 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        // 무게가 가정 적은 사람과 많은 사람을 같이 태워본다.
+        // 가장 무거운 사람 + 가장 가벼운 사람을 같이 태워본다.
         Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
         
-        int start = 0;
-        int end = people.length - 1;
-        
-        while (start <= end) {
-            if (people[start] + people[end] <= limit) {
+        while (left <= right) {
+            if (left == right) {
                 answer++;
-                start++;
-                end--;
+                break;
+            }
+            
+            if (people[right] + people[left] <= limit) {
+                answer++;
+                right--;
+                left++;
             } else {
-                // 맨 뒷사람만 태운다.
                 answer++;
-                end--;
+                right--;
             }
         }
         
