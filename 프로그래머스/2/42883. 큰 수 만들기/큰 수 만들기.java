b77280844ad771c9d3c3ed3b72e4abf6690c8a0b
dohee Kim <do151015@gmail.com>
2024-10-08 10:57:31 +0900

    [level 2] Title: 큰 수 만들기, Time: 7514.71 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,20 +1,21 @@
-class Solution {    
+class Solution {
     public String solution(String number, int k) {
-        char[] arr = number.toCharArray();
-        int start = 0;
         StringBuilder sb = new StringBuilder();
+        char max = '0';
+        int maxIdx = 0;
         
-        for (int i = 0; i < number.length() - k; i++) { // 뽑아야할 수만큼 반복
-            char max = '0';
-            for (int j = start; j <= k + i; j++) { // 뒤에 뽑아야할 수 - 1개 만큼은 남겨둔다.
-                if (max < arr[j]) {
-                    max = arr[j];
-                    start = j + 1;
+        for (int i = 0; i < number.length() - k; i++) { // 찾아야하는 수 만큼 반복
+            // 뒤에 찾아야하는 수 만큼 남겨두고 가장 큰 수를 찾는다.
+            for (int j = maxIdx; j <= k + i; j++) {
+                char c = number.charAt(j);
+                if (c > max) {
+                    max = c;
+                    maxIdx = j + 1;
                 }
             }
             sb.append(max);
+            max = '0';
         }
-        
         return sb.toString();
     }
 }
