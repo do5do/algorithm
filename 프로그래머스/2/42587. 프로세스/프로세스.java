35ac583a3eb69fa2dbf73b714669c0c967d655ee
dohee Kim <do151015@gmail.com>
2024-10-11 10:59:09 +0900

    [level 2] Title: 프로세스, Time: 1.23 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,29 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 1;
+        int answer = 0;
         Queue<int[]> queue = new LinkedList<>();
+        int[] arr = Arrays.copyOf(priorities, priorities.length);
+        Arrays.sort(arr);
+        int max = 0;
         
         for (int i = 0; i < priorities.length; i++) {
+            max = Math.max(priorities[i], max);
             queue.offer(new int[] {priorities[i], i});
         }
         
+        int idx = arr.length - 1;
+        
         while (!queue.isEmpty()) {
-            int max = 0;
-            for (int[] num : queue) {
-                max = Math.max(num[0], max);
-            }
+            int[] cur = queue.poll();
             
-            while (!queue.isEmpty() && queue.peek()[0] < max) {
-                queue.offer(queue.poll()); // 뒤로 넣기
+            if (cur[0] < arr[idx]) {
+                queue.offer(cur);
+            } else if (cur[0] == arr[idx]) {
+                answer++;
+                if (location == cur[1]) {
+                    return answer;
+                }
+                idx--;
+                
+                if (idx < 0) {
+                    break;
+                }
+            } else {
+                queue.offer(cur);
             }
-            
-            // max를 찾았으면
-            int[] maxNum = queue.poll();
-            if (maxNum[1] == location) {
-                return answer;
-            }
-            answer++;
         }
         
         return answer;
