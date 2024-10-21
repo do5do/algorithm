e51521745eae0cf059c7d3ddf61172734fe807e2
dohee Kim <do151015@gmail.com>
2024-10-21 10:46:54 +0900

    [level 2] Title: 배달, Time: 4.63 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,49 +2,42 @@ import java.util.*;
 
 class Solution {
     List<List<int[]>> graph = new ArrayList<>();
+    int[] dist;
     
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-        
+        int answer = 1; // 1번 포함
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
         
         for (int[] r : road) {
-            int a = r[0];
-            int b = r[1];
-            int c = r[2];
-            graph.get(a).add(new int[] {b, c});
-            graph.get(b).add(new int[] {a, c});
+            graph.get(r[0]).add(new int[] {r[1], r[2]});
+            graph.get(r[1]).add(new int[] {r[0], r[2]});
         }
-
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        bfs(dist, N);
+        dist = new int[N + 1];
+        bfs();
         
-        for (int i = 1; i < dist.length; i++) {
-            if (dist[i] <= K) {
+        for (int i = 2; i <= N; i++) {
+            if (dist[i] > 0 && dist[i] <= K) {
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void bfs(int[] dist, int n) {
+    public void bfs() {
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[1] - y[1]);
         pq.offer(new int[] {1, 0});
-        dist[1] = 0;
-        boolean[] visited = new boolean[n + 1];
         
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             
-            if (visited[cur[0]]) {
+            if (dist[cur[0]] != cur[1]) {
                 continue;
             }
-            visited[cur[0]] = true;
             
             for (int[] node : graph.get(cur[0])) {
                 if (dist[node[0]] > cur[1] + node[1]) {
