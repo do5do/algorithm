e0c86eaa5eba3b6ce424caa7ddadc93dd26e459e
dohee Kim <do151015@gmail.com>
2024-10-22 10:42:09 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 13.97 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,29 +1,21 @@
 class Solution {
     public int solution(int[][] board) {
-        int answer = 0;
+        int max = 0;
         
-        if (board.length <= 1 || board[0].length <= 1) {
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (board[i][j] == 1) {
-                        return 1;
-                    }
-                }
-            }
+        if (board.length == 1) {
+            return 1;
         }
         
         for (int i = 1; i < board.length; i++) {
             for (int j = 1; j < board[i].length; j++) {
-                if (board[i][j] != 0) {
-                    int min = Math.min(board[i - 1][j], board[i][j - 1]);
-                    min = Math.min(board[i - 1][j - 1], min);
-                    
-                    board[i][j] = min + 1;
-                    answer = Math.max(board[i][j], answer);
+                if (board[i][j] != 1) {
+                    continue;
                 }
+                int min = Math.min(board[i - 1][j], Math.min(board[i][j - 1], board[i - 1][j - 1]));
+                board[i][j] += min;
+                max = Math.max(board[i][j], max);
             }
         }
-        
-        return answer * answer;
+        return max * max;
     }
 }
