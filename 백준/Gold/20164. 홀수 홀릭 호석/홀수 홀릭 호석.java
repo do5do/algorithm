16b71b3ff36454e7862a156676cced9634ff96db
dohee Kim <do151015@gmail.com>
2024-10-28 10:07:32 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 124 ms, Memory: 16064 KB, Score: 6 point -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
@@ -1,20 +1,18 @@
 import java.io.*;
 
 public class Main {
-    static int oddCnt;
-    static String n;
     static int max = 0;
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = br.readLine();
-        go();
+        String n = br.readLine();
+        go(0, n);
         System.out.println(min + " " + max);
     }
 
-    public static void go() {
-        countOdd(); // 홀수 개수 확인
+    public static void go(int cnt, String n) {
+        int oddCnt = countOdd(n) + cnt; // 홀수 개수
 
         if (n.length() == 1) {
             max = Math.max(max, oddCnt);
@@ -23,22 +21,22 @@ public class Main {
             int first = n.charAt(0) - '0';
             int second = n.charAt(1) - '0';
             n = String.valueOf(first + second);
-            go();
+            go(oddCnt, n);
         } else if (n.length() >= 3) {
             for (int i = 0; i <= n.length() - 3; i++) {
                 for (int j = i + 1; j <= n.length() - 2; j++) {
-                    String first = n.substring(i, i + 1);
-                    String second = n.substring(j, j + 1);
+                    String first = n.substring(0, i + 1);
+                    String second = n.substring(i + 1, j + 1);
                     String third = n.substring(j + 1);
                     int sum = Integer.parseInt(first) + Integer.parseInt(second) + Integer.parseInt(third);
                     n = String.valueOf(sum);
-                    go();
+                    go(oddCnt, n);
                 }
             }
         }
     }
 
-    public static void countOdd() {
+    public static int countOdd(String n) {
         int cnt = 0;
         for (int i = 0; i < n.length(); i++) {
             int num = n.charAt(i) - '0';
@@ -46,6 +44,6 @@ public class Main {
                 cnt++;
             }
         }
-        oddCnt += cnt;
+        return cnt;
     }
 }
